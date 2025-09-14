024f6f761fd7226c73cd4be4e7f9f045d2669706
Shubham Ghori <shubhamghori97@gmail.com>
2025-09-14 17:39:29 +0530

    Converted footer into dynamic template with app configuration shared accross all site.

---
+++ app/app.config.ts
@@ -0,0 +1,41 @@
+export default defineAppConfig({
+  copyrightYear: "2025",
+  owner: {
+    name: "Jaydip Lakhankiya",
+    CompanyName: "Climate Walker",
+    email: "theclimatewalker@gmail.com",
+    social: {
+      YouTube: "https://www.youtube.com/@the.climate.walker",
+      Instagram: "https://www.instagram.com/the.climate.walker",
+      Facebook: "https://www.facebook.com/share/1EvQgo67RF/",
+      WhatsApp: "https://wa.me/917435827283"
+    }
+  },
+
+  navigationLinks: [
+    {
+      url: '/',
+      title: 'Home'
+    },
+    {
+      url: '/journey',
+      title: 'Journey'
+    },
+    {
+      url: '/about',
+      title: 'About'
+    },
+    {
+      url: '/gallary',
+      title: 'Gallery'
+    },
+    {
+      url: '/contact',
+      title: 'Contact'
+    },
+    {
+      url: '#',
+      title: 'Live Map'
+    },
+  ]
+})
