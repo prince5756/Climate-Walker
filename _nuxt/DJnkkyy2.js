6acec4e70b9538327fa83303f628ed3e24468203
Shubham Ghori <shubhamghori97@gmail.com>
2026-03-04 22:04:39 +0530

    Updates

--- _nuxt/DJnkkyy2.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./DA2Vul3x.js";import e from"./BP7hU-g_.js";import{x as n,j as s,o as a,w as i,l as p}from"./eB9ROpDL.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
